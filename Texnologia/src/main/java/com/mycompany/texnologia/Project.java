7bc00d54f3ef87e4be461242048b102137a6ccff
Lenos <43906906+LelouchViChristou@users.noreply.github.com>
2022-05-25 21:12:14 +0300

    Creation of Classes
    Created the basic classes and some methods

---
+++ Texnologia/src/main/java/com/mycompany/texnologia/Project.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.texnologia;
+
+/**
+ *
+ * @author chris
+ */
+public class Project {
+    String start_date ="";
+    String end_date ="";
+    String title ="";
+    String creator ="";
+    String freelancer ="";
+    String category ="";
+    String description="";
+    int state =0;
+}
